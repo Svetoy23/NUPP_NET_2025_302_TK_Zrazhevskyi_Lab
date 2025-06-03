2ff95449357fe5af042ebb483f442c6b74e26008
Svetoy23 <128981341+Svetoy23@users.noreply.github.com>
2025-06-03 21:57:53 +0300

    Delete lab 5 Zrazhevskyi directory

--- lab 5 Zrazhevskyi/Lab.Infrastructure/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Lab.Infrastructure.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        // Додаткові властивості можна додати тут
-    }
-}
